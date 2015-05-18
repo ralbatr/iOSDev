5d94e0e39334a77d3335aebbacc810ae41836c17
ralbatr <ralbatr@jbgsn.local>
2015-05-18 14:38:55 +0800

    第九次提交 计算类的编写
    计算类的编写

--- Calculator 应用MVC/Calculator/CalculatorBrain.swift
+++ Calculator 应用MVC/Calculator/CalculatorBrain.swift
@@ -12,10 +12,37 @@ class CalculatorBrain
 {
     enum Op {
         case Operand(Double)
-        case Operation(String,Double -> Double)
-        case SinaryOperation(Double,Double -> Double)
+        case UsaryOperation(String,Double -> Double)
+        case BinaryOperation(String,(Double,Double) -> Double)
     }
 //    var opStack = Array<ops>()
     var opStack = [Op]()
+    
+//    var knownOps = Dictionary<String,Op>()
+    var knownOps = [String:Op]()
+    
+    init() {
+//        knownOps["+"] = Op.BinaryOperation("+"){ $0 * $1 }
+//        knownOps["−"] = Op.BinaryOperation("−"){ $1 * $0 }
+//        knownOps["×"] = Op.BinaryOperation("×"){ $0 * $1 }
+//        knownOps["÷"] = Op.BinaryOperation("÷"){ $1 / $0 }
+//        knownOps["√"] = Op.UsaryOperation("√"){ sqrt($0) }
+        
+        knownOps["+"] = Op.BinaryOperation("+",+)
+        knownOps["−"] = Op.BinaryOperation("−"){ $1 * $0 }
+        knownOps["×"] = Op.BinaryOperation("×",*)
+        knownOps["÷"] = Op.BinaryOperation("÷"){ $1 / $0 }
+        knownOps["√"] = Op.UsaryOperation("√",sqrt)
+    }
+    
+    func pushOperand(operand:Double) {
+        opStack.append(Op.Operand(operand))
+    }
+    
+    func performOperation(symbol:String) {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+    }
     
 }
